e23fe3d97ce2f7a16ae7d76d8eb05c495cd88e62
BastienCNT <bastien.conti@wanadoo.fr>
2023-07-05 14:34:28 +0200

    updated mySQL

--- 3_Mon_Projet_Fil_Rouge/5_SQL/db.sql
+++ 3_Mon_Projet_Fil_Rouge/5_SQL/db.sql
@@ -3,29 +3,36 @@ use amalgame;
 
 create table user(
 id_user int primary key not null auto_increment
+-- ip? some id
 );
 
 create table gamedata(
--- id linked to user?
 id_mostkill int,
-id_nemesis int
+id_nemesis int,
+id_user int not null unique
 );
 
 create table monster(
 id_monster int primary key not null,
 name_monster varchar(50) not null unique,
 description_monster text not null,
-seen_monster tinyint(1),
-id_ability int, -- multiples !!!
-id_zone int
-);
-
-create table zone(
-id_zone int primary key not null auto_increment,
-name_zone varchar(50) not null unique
+seen_monster tinyint(1) default 0 not null,
+id_zone int not null
 );
 
 create table ability(
 id_ability int primary key not null auto_increment,
 name_ability varchar(50) not null unique
 );
+
+create table monster_abilities(
+id_monster int not null,
+id_ability int not null,
+primary key (id_monster,id_ability)
+);
+
+create table zone(
+id_zone int primary key not null auto_increment,
+name_zone varchar(50) not null unique
+);
+-- constraints
---
+++ 5_Exercices_En_Cours_Formation/MySQL/ticket_sql.sql
@@ -0,0 +1,126 @@
+-- créer la base de données
+
+create database ticket;
+use ticket;
+
+-- créer les tables
+
+create table product(
+id_product int primary key auto_increment not null,
+name_product varchar(50) not null,
+description_product text not null,
+price_product float not null,
+id_category int not null
+)Engine=InnoDB;
+
+create table category(
+id_category int primary key auto_increment not null,
+name_category varchar(50) not null
+)Engine=InnoDB;
+
+create table receipt(
+id_receipt int primary key auto_increment not null,
+date_receipt datetime not null,
+id_seller int not null
+)Engine=InnoDB;
+
+create table seller(
+id_seller int primary key auto_increment not null,
+name_seller varchar(50) not null,
+first_name_seller varchar(50) not null
+)Engine=InnoDB;
+
+create table to_add(
+id_product int not null,
+id_receipt int not null,
+qtx_add int not null,
+primary key(id_product, id_receipt)
+)Engine=InnoDB;
+
+-- Ajouter les contraintes Foreign key
+
+alter table product
+add constraint fk_completer_category
+foreign key(id_category)
+references category(id_category);
+
+alter table receipt
+add constraint fk_vendre_seller
+foreign key(id_seller)
+references seller(id_seller);
+
+alter table to_add
+add constraint fk_ajouter_produit
+foreign key(id_product)
+references product(id_product),
+add constraint fk_ajouter_ticket
+foreign key(id_receipt)
+references receipt(id_receipt);
+
+insert into category (name_category)
+values ("alimentaire"),("produit"),("loisir");
+
+insert into product (name_product,description_product,price_product,id_category )
+values ("tomate","paquet de 3kg de tomate", 4.20,1),
+("pain", "baguette de pain de 350 gr", 0.90, 1),
+("lessive", "paquet de lessive de 1 kg", 14.30, 2),
+("livre", "livre sur le cinéma", 29.99, 3);
+
+insert into seller(name_seller,firstname_seller)
+values ("Dupond","Sophie"),("Albert","Marc");
+
+insert into receipt(date_receipt,id_seller)
+values ("2023-02-03", 1),("2023-05-06", 2), ("2023-07-14", 2);
+
+insert into toadd(id_product, id_receipt,qtx_add)VALUES
+(2,13,5),(1,15,2),(3,14,1),(4,13,2);
+
+UPDATE category SET name_category = 'alimentation'
+WHERE name_category = "alimentaire";
+
+UPDATE category SET name_category = 'autre'
+WHERE id_category = 2;
+
+UPDATE product SET price_product = 34.99
+WHERE name_product = "livre";
+
+UPDATE product SET description_product = "Lessive liquide 40 lavages"
+WHERE name_product = "lessive";
+
+UPDATE product SET price_product = price_product+1;
+-- WHERE name_product = "lessive";
+
+UPDATE seller 
+SET name_seller = "Dupont",
+	firstname_seller = "Anne"
+WHERE id_seller = 1;
+
+UPDATE seller 
+SET firstname_seller = "Maxime"
+WHERE name_seller = "Albert";
+
+UPDATE receipt 
+SET date_receipt = "2023-06-23"
+WHERE id_receipt = 14;
+
+UPDATE receipt 
+SET id_seller = 1
+WHERE date_receipt = "2023-07-14";
+
+UPDATE toadd 
+SET qtx_add = qtx_add+2
+WHERE id_product = 13;
+
+Update toadd
+SET id_product = 
+(select id_product from product where name_product ="lessive")
+WHERE id_receipt = 15 
+and id_product = (select id_product from product where name_product ="tomate");
+
+Update toadd
+SET qtx_add = 5
+WHERE id_receipt = 15 
+and id_product = (select id_product from product where name_product ="tomate");
+
+
+
