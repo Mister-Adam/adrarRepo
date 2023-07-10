52b8506fdc65f2b043ec33309ca0c9741a5b4adf
BastienCNT <bastien.conti@wanadoo.fr>
2023-07-10 16:01:44 +0200

    update fil rouge

--- 3_Mon_Projet_Fil_Rouge/5_SQL/db.sql
+++ 3_Mon_Projet_Fil_Rouge/5_SQL/db.sql
@@ -7,10 +7,19 @@ key_user varchar(250) not null unique
 -- ip? some id
 );
 
+create table article(
+id_article int primary key not null auto_increment,
+title_article varchar(50) not null,
+content_article text not null,
+date_article date not null,
+status_article bool, 
+type_article bool
+);
+
 create table gamedata(
 id_gamedata int primary key not null auto_increment,
-datetime_update_gamedata datetime,
-ingame_time_gamedata time
+datetime_update_gamedata datetime not null,
+ingame_time_gamedata time not null
 );
 
 create table monster(
@@ -18,20 +27,25 @@ id_monster int primary key not null,
 name_monster varchar(50) not null unique,
 amount_killed_monster int unsigned default 0,
 amount_died_to_monster int unsigned default 0,
+chance_see_monster int8 not null,
 id_arena int not null,
 id_rank int not null,
 id_class int not null
 );
+
+-- change to not null
 
 create table arena(
 id_arena int primary key not null auto_increment,
 name_arena varchar(50) not null
 );
 
+
 create table monster_rank(
 id_monster_rank int primary key not null auto_increment,
 name_monster_rank varchar(50) not null
 );
+
 
 create table monster_class(
 id_monster_class int primary key not null auto_increment,
@@ -50,11 +64,27 @@ id_ability int not null,
 primary key (id_monster,id_ability)
 );
 
+-- add amounts here?
 create table monster_data(
 id_monster int not null,
 id_game_data int not null,
+amount_killed_monster int unsigned default 0,
+amount_died_to_monster int unsigned default 0,
 primary key (id_monster,id_game_data)
 );
 
 
--- constraints
+create table monster_images(
+id_monster int not null,
+id_image int not null,
+primary key (id_monster,id_image)
+);
+
+create table article_images(
+id_article int not null,
+id_image int not null,
+primary key (id_article,id_image)
+);
+
+-- user_data
+-- article_img
